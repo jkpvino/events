a6fdbe352c475019ced86651492cd8acfc1d34fe
jkpvino92 <jkpvino92@gmail.com>
2019-11-15 08:42:28 +0530

    Description : Event view page

--- application/views/event.php
+++ application/views/event.php
@@ -1,11 +1,325 @@
-
 <!-- Page Content -->
-<div id="page-content">
+<div id="page-content" style="">
     <div class="container">
-        <div class="row">
-            
-            
+        <div class="row" style="margin-top:25px; ">
+            <!-- Course Image -->
+            <div class="col-md-2 col-sm-3">
+                <figure class="event-image">
+                    <div class="image-wrapper"><img src="<?php echo base_url() ?>assets/img/course-detail-img.jpg"></div>
+                </figure>
+            </div>
+            <!-- end Course Image -->
+        	<div class="col-md-8 col-sm-9">
+                <div id="page-main">
+		        	<section id="event-detail">
+		                <article class="event-detail">
+		                    <section id="event-header">
+		                        <header>
+		                            <h2 class="event-date">Saturday March 27, 2015</h2>
+		                        </header>
+		                        <hr>
+		                        <div class="course-count-down" id="course-count-down">
+		                            <!-- /.course-start -->
+		                            <div class="count-down-wrapper">
+		                            	<div class="count-down-wrapper">AARON 2K11 SYMPOSIUM </div>
+		                            	<p> SACS MAVMM ENGG COLLEGE MADURAI </p>
+		                                <script type="text/javascript">var _date = 'Jun 27, 2015 23:28';</script>
+		                            </div><!-- /.count-down-wrapper -->
+
+		                        </div><!-- /.course-count-down -->
+		                        <hr>
+		                        <figure>
+		                            <span class="course-summary" id="course-length"><i class="fa fa-mobile"></i>+91 8220466675</span>
+		                            <span class="course-summary" id="course-time-amount"><i class="fa fa-envelope"></i>vinothkumarjeyaraman@gmail.com</span>
+		                            <span class="course-summary" id="course-time-amount"><i class="fa fa-map-marker"></i><a href=""> Direction </a> </span>
+		                            <span class="course-summary" id="course-course-time"><i class="fa fa-clock-o"></i>6:00pm – 8:00pm</span>
+		                        </figure>
+		                    </section><!-- /#course-header -->
+		                </article>
+		            </section>
+		        </div>
+		    </div>            
         </div><!-- /.row -->
+        <div class="row">
+            <section>
+            	<div>
+	                <header><h2>Event Info</h2></header>
+	                <p>
+	                    International Conference on Recent Advances in Medical, Medicine and Health Sciences (ICRAMMHS)  will be held on 14th November, 2019 at  Chennai, India.  ICRAMMHS  is to bring together innovative academics and industrial experts in the field of Medical, Medicine and Health Sciences to a common forum. All the registered papers will be published by the World Research Library and will be submitted for review for indexing by Google Scholar etc.
+	                </p>
+	                <p>
+	                    This Conference is sponsored by  many International institutes. The conference would offer a large number of invited lectures from renowned speakers all over the country. The Best paper awards will be given for the papers judged to make the most significant contribution to the conference.
+	                </p>
+	                <p>
+	                    All submissions to the conference will be reviewed by at least two independent peers for technical merit and content. It is anticipated that a broad range of research and applied topics will be covered during the conference.
+	                </p>
+                </div>
+
+
+
+                <div class="row col-md-12">
+                	<div class=""><img style="width: 100%" src="<?php echo base_url() ?>assets/img/sym_banner.jpg"></div>
+                </div>
+
+
+
+
+
+                    <section class="events images" id="events">
+                        <header><h2> Events</h2></header>
+                        <div class="section-content">
+                            <article class="event">
+                                <div class="event-thumbnail">
+                                    <figure class="event-image">
+                                        <div class="image-wrapper"><img src="<?php echo base_url() ?>assets/img/event-img-01.jpg"></div>
+                                    </figure>
+                                    <figure class="date">
+                                        <div class="month">jan</div>
+                                        <div class="day">18</div>
+                                    </figure>
+                                </div>
+                                <aside>
+                                    <header>
+                                        <a href="event-detail.html">Conservatory Exhibit: The garden of india a country and culture revealed</a>
+                                    </header>
+                                    <div class="additional-info"><span class="fa fa-map-marker"></span> Matthaei Botanical Gardens</div>
+                                    <div class="description">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et urna fringilla
+                                            volutpat elit non, tristique lectus. Nam blandit odio nisl, ac malesuada lacus fermentum sit amet.
+                                            Vestibulum vitae aliquet felis, ornare feugiat elit. Nulla varius condimentum elit,
+                                            sed pulvinar leo sollicitudin vel.
+                                        </p>
+                                    </div>
+                                    <a href="event-detail.html" class="btn btn-framed btn-color-grey btn-small">View Details</a>
+
+
+
+                                        <section id="contact" style="display: block;">
+					                        <div class="row">
+					                            <div class="col-md-12">
+					                                <address>
+					                                    <h3>Contactus</h3>
+					                                    <span>Vinoth Kumar J</span>
+					                                    <br>
+					                                    <span>West Newton, PA 15089</span>
+					                                    <br>
+					                                    <abbr title="Telephone">Telephone:</abbr> +1 (734) 123-4567
+					                                    <br>
+					                                    <abbr title="Email">Email:</abbr> <a href="#">questions@youruniversity.com</a>
+					                                </address>
+					                                <div class="icons">
+					                                    <a href=""><i class="fa fa-twitter"></i></a>
+					                                    <a href=""><i class="fa fa-facebook"></i></a>
+					                                    <a href=""><i class="fa fa-pinterest"></i></a>
+					                                    <a href=""><i class="fa fa-youtube-play"></i></a>
+					                                </div><!-- /.icons -->
+					                                <hr>
+					                                <p>
+					                                    Morbi rutrum massa eget mi gravida, sit amet mollis magna gravida. Morbi sodales,
+					                                    ligula quis ornare bibendum, magna erat convallis ipsum, id posuere ligula massa vitae leo.
+					                                </p>
+					                            </div>
+					                        </div>
+					                    </section>
+                                </aside>
+                            </article><!-- /.event -->
+                            <article class="event">
+                                <div class="event-thumbnail">
+                                    <figure class="event-image">
+                                        <div class="image-wrapper"><img src="<?php echo base_url() ?>assets/img/event-img-02.jpg"></div>
+                                    </figure>
+                                    <figure class="date">
+                                        <div class="month">feb</div>
+                                        <div class="day">01</div>
+                                    </figure>
+                                </div>
+                                <aside>
+                                    <header>
+                                        <a href="event-detail.html">February Half-Term Activities: Big Stars and Little Secrets </a>
+                                    </header>
+                                    <div class="additional-info"><span class="fa fa-map-marker"></span> Pitt Rivers and Natural History Museums</div>
+                                    <div class="description">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et urna fringilla
+                                            volutpat elit non, tristique lectus. Nam blandit odio nisl, ac malesuada lacus fermentum sit amet.
+                                            Vestibulum vitae aliquet felis, ornare feugiat elit. Nulla varius condimentum elit,
+                                            sed pulvinar leo sollicitudin vel.
+                                        </p>
+                                    </div>
+                                    <a href="event-detail.html" class="btn btn-framed btn-color-grey btn-small">View Details</a>
+                                </aside>
+                            </article><!-- /.event -->
+                            <article class="event">
+                                <div class="event-thumbnail">
+                                    <figure class="event-image">
+                                        <div class="image-wrapper"><img src="<?php echo base_url() ?>assets/img/event-img-03.jpg"></div>
+                                    </figure>
+                                    <figure class="date">
+                                        <div class="month">mar</div>
+                                        <div class="day">23</div>
+                                    </figure>
+                                </div>
+                                <aside>
+                                    <header>
+                                        <a href="event-detail.html">The Orchestra of the Age of Enlightenment perform with Music</a>
+                                    </header>
+                                    <div class="additional-info"><span class="fa fa-map-marker"></span> Faculty of Music</div>
+                                    <div class="description">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et urna fringilla
+                                            volutpat elit non, tristique lectus. Nam blandit odio nisl, ac malesuada lacus fermentum sit amet.
+                                            Vestibulum vitae aliquet felis, ornare feugiat elit. Nulla varius condimentum elit,
+                                            sed pulvinar leo sollicitudin vel.
+                                        </p>
+                                    </div>
+                                    <a href="event-detail.html" class="btn btn-framed btn-color-grey btn-small">View Details</a>
+                                </aside>
+                            </article><!-- /.event -->
+                        </div><!-- /.section-content -->
+                    </section><!-- /.events-images -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	            <section class="events" id="events">
+	            	<header><h1>Events</h1></header>
+
+	            	<div class="section-content">
+	                    <article class="event">
+	                        <figure class="date">
+	                            <div class="month">jan</div>
+	                            <div class="day">18</div>
+	                        </figure>
+	                        <aside>
+	                            <header>
+	                                <a href="event-detail.html">Conservatory Exhibit: The garden of india a country and culture revealed</a>
+	                            </header>
+	                            <div class="additional-info"><span class="fa fa-map-marker"></span> Matthaei Botanical Gardens</div>
+	                            <div class="description">
+	                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et urna fringilla
+	                                    volutpat elit non, tristique lectus. Nam blandit odio nisl, ac malesuada lacus fermentum sit amet.
+	                                    Vestibulum vitae aliquet felis, ornare feugiat elit. Nulla varius condimentum elit,
+	                                    sed pulvinar leo sollicitudin vel.
+	                                </p>
+	                            </div>
+	                            <a href="event-detail.html" class="btn btn-framed btn-color-grey btn-small">View Details</a>
+	                        </aside>
+	                    </article><!-- /article -->
+	                    <article class="event">
+	                        <figure class="date">
+	                            <div class="month">feb</div>
+	                            <div class="day">01</div>
+	                        </figure>
+	                        <aside>
+	                            <header>
+	                                <a href="event-detail.html">February Half-Term Activities: Big Stars and Little Secrets </a>
+	                            </header>
+	                            <div class="additional-info"><span class="fa fa-map-marker"></span> Pitt Rivers and Natural History Museums</div>
+	                            <div class="description">
+	                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et urna fringilla
+	                                    volutpat elit non, tristique lectus. Nam blandit odio nisl, ac malesuada lacus fermentum sit amet.
+	                                    Vestibulum vitae aliquet felis, ornare feugiat elit. Nulla varius condimentum elit,
+	                                    sed pulvinar leo sollicitudin vel.
+	                                </p>
+	                            </div>
+	                            <a href="event-detail.html" class="btn btn-framed btn-color-grey btn-small">View Details</a>
+	                        </aside>
+	                    </article><!-- /article -->
+	                    <article class="event">
+	                        <figure class="date">
+	                            <div class="month">mar</div>
+	                            <div class="day">23</div>
+	                        </figure>
+	                        <aside>
+	                            <header>
+	                                <a href="event-detail.html">The Orchestra of the Age of Enlightenment perform with Music</a>
+	                            </header>
+	                            <div class="additional-info"><span class="fa fa-map-marker"></span> Faculty of Music</div>
+	                            <div class="description">
+	                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et urna fringilla
+	                                    volutpat elit non, tristique lectus. Nam blandit odio nisl, ac malesuada lacus fermentum sit amet.
+	                                    Vestibulum vitae aliquet felis, ornare feugiat elit. Nulla varius condimentum elit,
+	                                    sed pulvinar leo sollicitudin vel.
+	                                </p>
+	                            </div>
+	                            <a href="event-detail.html" class="btn btn-framed btn-color-grey btn-small">View Details</a>
+	                        </aside>
+	                    </article><!-- /article -->
+	                    <article class="event">
+	                        <figure class="date">
+	                            <div class="month">may</div>
+	                            <div class="day">12</div>
+	                        </figure>
+	                        <aside>
+	                            <header>
+	                                <a href="event-detail.html">Museums and the Construction of Identities</a>
+	                            </header>
+	                            <div class="additional-info"><span class="fa fa-map-marker"></span> Faculty of Music</div>
+	                            <div class="description">
+	                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et urna fringilla
+	                                    volutpat elit non, tristique lectus. Nam blandit odio nisl, ac malesuada lacus fermentum sit amet.
+	                                    Vestibulum vitae aliquet felis, ornare feugiat elit. Nulla varius condimentum elit,
+	                                    sed pulvinar leo sollicitudin vel.
+	                                </p>
+	                            </div>
+	                            <a href="event-detail.html" class="btn btn-framed btn-color-grey btn-small">View Details</a>
+	                        </aside>
+	                    </article><!-- /article -->
+	                    <article class="event">
+	                        <figure class="date">
+	                            <div class="month">jun</div>
+	                            <div class="day">17</div>
+	                        </figure>
+	                        <aside>
+	                            <header>
+	                                <a href="event-detail.html">Reporting: In Conversation with Miriam Elder and Julia Ioffe</a>
+	                            </header>
+	                            <div class="additional-info"><span class="fa fa-map-marker"></span> Faculty of Music</div>
+	                            <div class="description">
+	                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et urna fringilla
+	                                    volutpat elit non, tristique lectus. Nam blandit odio nisl, ac malesuada lacus fermentum sit amet.
+	                                    Vestibulum vitae aliquet felis, ornare feugiat elit. Nulla varius condimentum elit,
+	                                    sed pulvinar leo sollicitudin vel.
+	                                </p>
+	                            </div>
+	                            <a href="event-detail.html" class="btn btn-framed btn-color-grey btn-small">View Details</a>
+	                        </aside>
+	                    </article><!-- /article -->
+	                </div>
+
+
+
+	            </section>
+            </section>
+            <!-- /#course-info -->
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div><!-- /.container -->
 </div>
 <!-- end Page Content -->
