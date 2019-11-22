befe6f67b3d823d890148f65e6856533fe2e6726
jkpvino92 <jkpvino92@gmail.com>
2019-11-23 01:00:15 +0530

    Description : Event list page

--- application/views/events.php
+++ application/views/events.php
@@ -96,7 +96,7 @@
                                     <h5 class="mb-2">Conference</h5>
                                     <hr class="m-0">
                                     <a href="/e/kpjot3g4/india-atp-annual-conference">
-                                        <h5 class="text-right mb-0 mt-2"> Free <i class="fa fa-long-arrow-right" aria-hidden="true"></i></h5>
+                                        <h5 class="text-right mb-0 mt-2"> Readmore <i class="fa fa-long-arrow-right" aria-hidden="true"></i></h5>
                                     </a>
                                 </div>
                             </div>
@@ -110,6 +110,14 @@
 
 
 <style type="text/css">
+    #homepage-carousel{
+        background:
+linear-gradient(45deg, #92baac 45px, transparent 45px)64px 64px,
+linear-gradient(45deg, #92baac 45px, transparent 45px,transparent 91px, #e1ebbd 91px, #e1ebbd 135px, transparent 135px),
+linear-gradient(-45deg, #92baac 23px, transparent 23px, transparent 68px,#92baac 68px,#92baac 113px,transparent 113px,transparent 158px,#92baac 158px);
+background-color:#e1ebbd;
+background-size: 128px 128px;
+    }
 .lgx-single-news {
     margin-bottom: 2.6rem;
     padding: 15px;
@@ -145,7 +153,7 @@
 
                             <?php foreach ($sympos as $key => $value) { 
                                 $i=1;
-                                if($i%2 != 0){
+                                if($i%3 != 0){
                                 ?>
                             <div class="row">
                             <?php } ?>
@@ -168,7 +176,7 @@
                                     </aside>
                                 </article><!-- /article -->
                             </div><!-- /.col-md-6 -->
-                            <?php if($i%2 != 0){
+                            <?php if($i%3 != 0){
                                 ?>
                               </div><!-- /.row -->
                           <?php } 
@@ -183,81 +191,6 @@
                 </div><!-- /#page-main -->
             </div><!-- /.col-md-8 -->
 
-            <!--SIDEBAR Content-->
-            <div class="col-md-4">
-                <div id="page-sidebar" class="sidebar">
-                    <aside class="news-small" id="news-small">
-                        <header>
-                            <h2>Related News</h2>
-                        </header>
-                        <div class="section-content">
-                            <article>
-                                <figure class="date"><i class="fa fa-file-o"></i>08-24-2014</figure>
-                                <header><a href="#">U-M School of Public Health, Detroit partners aim to improve air quality in the city</a></header>
-                            </article><!-- /article -->
-                            <article>
-                                <figure class="date"><i class="fa fa-file-o"></i>08-24-2014</figure>
-                                <header><a href="#">At 50, Center for the Education of Women celebrates a wider mission</a></header>
-                            </article><!-- /article -->
-                            <article>
-                                <figure class="date"><i class="fa fa-file-o"></i>08-24-2014</figure>
-                                <header><a href="#">Three U-Michigan scientists receive Sloan fellowships</a></header>
-                            </article><!-- /article -->
-                        </div><!-- /.section-content -->
-                        <a href="" class="read-more">All News</a>
-                    </aside><!-- /.news-small -->
-                    <aside id="newsletter">
-                        <header>
-                            <h2>Newsletter</h2>
-                            <div class="section-content">
-                                <div class="newsletter">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control" placeholder="Your e-mail">
-                                    <span class="input-group-btn">
-                                        <button type="submit" class="btn"><i class="fa fa-angle-right"></i></button>
-                                    </span>
-                                    </div><!-- /input-group -->
-                                </div><!-- /.newsletter -->
-                                <p class="opacity-50">Ut tincidunt, quam in tincidunt vestibulum, turpis ipsum porttitor nisi, et fermentum augue
-                                    lit eu neque. In at tempor dolor, sit amet dictum lacus. Praesent porta orci eget laoreet ultrices.
-                                </p>
-                            </div><!-- /.section-content -->
-                        </header>
-                    </aside><!-- /.newsletter -->
-                    <aside id="our-professors">
-                        <header>
-                            <h2>Our Professors</h2>
-                        </header>
-                        <div class="section-content">
-                            <div class="professors">
-                                <article class="professor-thumbnail">
-                                    <figure class="professor-image"><a href="member-detail.html"><img src="assets/img/professor.jpg" alt=""></a></figure>
-                                    <aside>
-                                        <header>
-                                            <a href="member-detail.html">Prof. Mathew Davis</a>
-                                            <div class="divider"></div>
-                                            <figure class="professor-description">Applied Science and Engineering</figure>
-                                        </header>
-                                        <a href="member-detail.html" class="show-profile">Show Profile</a>
-                                    </aside>
-                                </article><!-- /.professor-thumbnail -->
-                                <article class="professor-thumbnail">
-                                    <figure class="professor-image"><a href="#"><img src="assets/img/professor-02.jpg" alt=""></a></figure>
-                                    <aside>
-                                        <header>
-                                            <a href="member-detail.html">Prof. Jane Stairway</a>
-                                            <div class="divider"></div>
-                                            <figure class="professor-description">Applied Science and Engineering</figure>
-                                        </header>
-                                        <a href="member-detail.html" class="show-profile">Show Profile</a>
-                                    </aside>
-                                </article><!-- /.professor-thumbnail -->
-                                <a href="" class="read-more">All Professors</a>
-                            </div><!-- /.professors -->
-                        </div><!-- /.section-content -->
-                    </aside><!-- /.our-professors -->
-                </div><!-- /#sidebar -->
-            </div><!-- /.col-md-4 -->
         </div><!-- /.row -->
     </div><!-- /.container -->
 </div>
