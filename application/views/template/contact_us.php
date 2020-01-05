0a238d34712261a3feb8f37368991d42ceaeea91
jkpvino92 <jkpvino92@gmail.com>
2020-01-05 13:11:36 +0530

    Event List

--- application/views/template/contact_us.php
+++ application/views/template/contact_us.php
@@ -1,122 +1,70 @@
-<div class="contactus_container">
-  
-  
-  <!-- <div class="text-center">
-    <button type="button" class="btn btn-demo" data-toggle="modal" data-target="#myModal">
-      Left Sidebar Modal
-    </button>
-
-    <button type="button" class="btn btn-demo" data-toggle="modal" data-target="#contactus_form">
-      Right Sidebar Modal
-    </button>
-  </div> -->
-
-  <!-- Modal 
-  <div class="modal left fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title" id="myModalLabel">Left Sidebar</h4>
-        </div>
-
-        <div class="modal-body">
-          <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-          </p>
-        </div>
-
-      </div>
-    </div>
-  </div>-->
-  
+<div class="contactus_container">  
   <!-- Modal -->
-  <div class="modal right fade" id="contactus_form" tabindex="-1" role="dialog" aria-labelledby="contactus_block">
+  <div class="modal right fade contactus_form" id="contactus_form" tabindex="-1" role="dialog" aria-labelledby="contactus_block">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
-
+        <form method="post" action="" name="contact_us" id="contact_us_form"> 
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title" id="contactus_block">Get In Touch</h4>
+          <h4 class="modal-title" id="contactus_block">Get in touch with us</h4>
         </div>
 
         <div class="modal-body">
-          <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-          </p>
+          <div class="row"> 
+              <div class="col-md-12 contact_header">
+                  <p style="color: #fff;"> Whether you have a question about features, advertisement, sponsors or anything else, our team is ready to answer all your questions </p>
+                  <img src="<?php echo base_url() ?>assets/img/title-separator.png">
+                  <br>
+              </div>
+          </div>
+          <div class="row"> 
+              <div class="col-md-12">
+                  <div class="input-group">
+                      <div><label class="control-label"> Full Name  </label> </div>
+                      <input class="form-control has-dark-background" required name="fullname" id="fullname" placeholder="Enter Full Name" type="text" value="">
+                  </div>
+              </div><!-- /.col-md-6 -->
+          </div><!-- /.row -->
+          <div class="row"> 
+              <div class="col-md-12">
+                  <div class="input-group">
+                      <div><label class="control-label"> Email  </label> </div>
+                      <input class="form-control has-dark-background" required name="email" id="email" placeholder="Enter Email Address" type="email">
+                  </div>
+              </div><!-- /.col-md-6 -->
+          </div><!-- /.row -->
+          <div class="row">
+            <div class="col-md-12">
+                <div class="input-group">
+                    <div><label class="control-label"> Mobile No  </label></div>
+                    <div class="input-group date datetimepicker" >
+                        <input id='mobileno' name="mobileno" required type='text' class="form-control has-dark-background" placeholder="Enter your mobile number"  />
+                        <span class="input-group-addon">
+                            <span class="glyphicon glyphicon-phone"></span>
+                        </span>
+                    </div>
+                </div>
+            </div><!-- /.col-md-6 -->
+          </div>
+          <div class="row"> 
+              <div class="col-md-12">
+                  <div class="input-group">
+                      <div><label class="control-label"> Your Message  </label> </div>
+                      <textarea class="form-control has-dark-background" required rows="5"> </textarea>
+                      <!-- <input class="form-control" name="program_name" id="program_name" placeholder="Event Name" type="text"  value=""> -->
+                  </div>
+              </div><!-- /.col-md-6 -->
+          </div><!-- /.row -->
+          <div class="row"> 
+              <div class="col-md-12">
+                  <div class="input-group">
+                      <button class="btn btn-framed"> Submit </button>            
+                  </div>
+              </div><!-- /.col-md-6 -->
+          </div><!-- /.row -->
         </div>
-
+        </form>
       </div><!-- modal-content -->
     </div><!-- modal-dialog -->
   </div><!-- modal -->
-  
-  
 </div><!-- container -->
-
-
-<style type="text/css">
-/*******************************
-* MODAL AS LEFT/RIGHT SIDEBAR
-* Add "left" or "right" in modal parent div, after class="modal".
-* Get free snippets on bootpen.com
-*******************************/
-  .modal.left .modal-dialog,
-  .modal.right .modal-dialog {
-    position: fixed;
-    margin: auto;
-    width: 320px;
-    height: 100%;
-    -webkit-transform: translate3d(0%, 0, 0);
-        -ms-transform: translate3d(0%, 0, 0);
-         -o-transform: translate3d(0%, 0, 0);
-            transform: translate3d(0%, 0, 0);
-  }
-
-  .modal.left .modal-content,
-  .modal.right .modal-content {
-    height: 100%;
-    overflow-y: auto;
-  }
-  
-  .modal.left .modal-body,
-  .modal.right .modal-body {
-    padding: 15px 15px 80px;
-  }
-
-/*Left*/
-  .modal.left.fade .modal-dialog{
-    left: -320px;
-    -webkit-transition: opacity 0.3s linear, left 0.3s ease-out;
-       -moz-transition: opacity 0.3s linear, left 0.3s ease-out;
-         -o-transition: opacity 0.3s linear, left 0.3s ease-out;
-            transition: opacity 0.3s linear, left 0.3s ease-out;
-  }
-  
-  .modal.left.fade.in .modal-dialog{
-    left: 0;
-  }
-        
-/*Right*/
-  .modal.right.fade .modal-dialog {
-    right: -320px;
-    -webkit-transition: opacity 0.3s linear, right 0.3s ease-out;
-       -moz-transition: opacity 0.3s linear, right 0.3s ease-out;
-         -o-transition: opacity 0.3s linear, right 0.3s ease-out;
-            transition: opacity 0.3s linear, right 0.3s ease-out;
-  }
-  
-  .modal.right.fade.in .modal-dialog {
-    right: 0;
-  }
-
-/* ----- MODAL STYLE ----- */
-  .modal-content {
-    border-radius: 0;
-    border: none;
-  }
-
-  .modal-header {
-    border-bottom-color: #EEEEEE;
-    background-color: #FAFAFA;
-  }
-
-</style>
